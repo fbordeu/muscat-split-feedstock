767b95378431f8990fc4fdd555044d798232274e
BORDEU Felipe <felipe.bordeu@safrangroup.com>
2025-02-10 17:12:20 +0100

    Move 2.4

--- recipe/bld_core.bat
+++ recipe/bld_core.bat
@@ -1,4 +1,25 @@
-%python% setup.py build_clib 
-if errorlevel 1 exit 1 
-%python% -m pip install --no-deps . -vv 
+setlocal EnableDelayedExpansion
+
+mkdir build
+cd build
+
+
+cmake .. -G "Ninja"                            ^
+-D CMAKE_BUILD_TYPE=Release                    ^
+-D CMAKE_EXPORT_COMPILE_COMMANDS:BOOL="TRUE"   ^
+-D Muscat_ENABLE_Kokkos:BOOL=ON                ^
+-D Muscat_ENABLE_Python:BOOL=ON                ^
+-D Muscat_ENABLE_Documentation=OFF             ^
+-D mmg_DIR:PATH=%PREFIX%/lib/cmake/mmg         ^
+-D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"        ^
+-D CMAKE_INSTALL_LIBDIR="Library/lib"          ^
+-D CMAKE_INSTALL_BINDIR="Library/bin"          ^
+-D CMAKE_INSTALL_INCLUDEDIR="Library/include"  ^
+-D CMAKE_INSTALL_DATAROOTDIR="Library/share"   ^
+-D CMAKE_INSTALL_PREFIX="%PREFIX%"
+
+
+%python% setup.py build_clib
+if errorlevel 1 exit 1
+%python% -m pip install --no-deps . -vv
 if errorlevel 1 exit 1
--- recipe/build_core.sh
+++ recipe/build_core.sh
@@ -1,3 +1,28 @@
-set -x 
-$PYTHON setup.py build_clib 
-$PYTHON -m pip install --no-deps . -vv
+set -ex
+
+BUILD_CONFIG=Release
+
+mkdir cmakeBuild
+
+cmake  ${CMAKE_ARG}                            \
+-D CMAKE_BUILD_TYPE=${BUILD_CONFIG}            \
+-D CMAKE_EXPORT_COMPILE_COMMANDS:BOOL="TRUE"   \
+-D Muscat_ENABLE_Kokkos:BOOL=ON                \
+-D Muscat_ENABLE_Python:BOOL=ON                \
+-D Muscat_ENABLE_Documentation=OFF             \
+-D mmg_DIR:PATH=%PREFIX%/lib/cmake/mmg         \
+-D CMAKE_INSTALL_PREFIX=${PREFIX}              \
+-G Ninja                                       \
+-B ${PWD}/cmakeBuild                           \
+-S .
+
+cmake                 \
+--build cmakeBuild    \
+--parallel 16
+
+ctest                  \
+--test-dir cmakeBuild  \
+--output-on-failure    \
+--parallel 16
+
+cmake --install cmakeBuild
