86982552776aef35e299374d9e5899e58d95bdf2
BORDEU Felipe <felipe.bordeu@safrangroup.com>
2025-06-26 16:12:36 +0200

    Enable cross compilation

--- recipe/build_core.sh
+++ recipe/build_core.sh
@@ -5,6 +5,17 @@ BUILD_DIR=cmakeBuild${PY_VER}
 
 mkdir -p ${BUILD_DIR}
 
+if [[ $target_platform != $build_platform ]]; then
+    Python_INCLUDE_DIR="$(python -c 'import sysconfig; print(sysconfig.get_path("include"))')"
+    Python_NumPy_INCLUDE_DIR="$(python -c 'import numpy;print(numpy.get_include())')"
+    CMAKE_ARGS="${CMAKE_ARGS} -DPython_EXECUTABLE:PATH=${PYTHON}"
+    CMAKE_ARGS="${CMAKE_ARGS} -DPython_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
+    CMAKE_ARGS="${CMAKE_ARGS} -DPython_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
+else
+    CMAKE_ARGS="${CMAKE_ARGS} -D Python_EXECUTABLE=${PYTHON}"
+fi
+
+
 cmake  ${CMAKE_ARG}                            \
 -D CMAKE_BUILD_TYPE=${BUILD_CONFIG}            \
 -D CMAKE_EXPORT_COMPILE_COMMANDS:BOOL="TRUE"   \
@@ -12,10 +23,7 @@ cmake  ${CMAKE_ARG}                            \
 -D Muscat_ENABLE_Python:BOOL=ON                \
 -D Muscat_ENABLE_Documentation=OFF             \
 -D mmg_DIR:PATH=${PREFIX}/lib/cmake/mmg        \
--D PYTHON_EXECUTABLE="${PYTHON}"               \
--D PYTHON3_EXECUTABLE="${PYTHON}"              \
--D Python3_EXECUTABLE="${PYTHON}"              \
--D Python_EXECUTABLE="${PYTHON}"               \
+
 -D CMAKE_INSTALL_PREFIX=${PREFIX}              \
 -G Ninja                                       \
 -B ${PWD}/${BUILD_DIR}                         \
